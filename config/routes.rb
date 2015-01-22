06e163eb5100cbb733d7e603da29b499992365f1
Mehmet Duran <mehmet.duran@gmail.com>
2015-01-22 13:04:13 +0000

    Cleaning up leftover code

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :reports, only: [:new, :create, :index, :edit, :update] do
-    get :approve, on: :member
-    get :mid_year_approve, on: :member
-
     resources :approvals, only: [:edit, :update], constraints: { id: /initial|mid_year|end_year/ }
     resources :reviews, only: [:edit, :update], constraints: { id: /mid_year|end_year/ }
   end
