a8960a269915d481b76b96e7a446c1b56226cc88
Jakub Novotny <novotny.jakub@gmail.com>
2015-01-22 14:48:22 +0000

    Fix unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,7 +3,7 @@ worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
 
-before_fork do ||
+before_fork do |_, _|
   Signal.trap 'TERM' do
     Rails.logger.info 'Unicorn master intercepting TERM and
       sending myself QUIT instead'
@@ -13,7 +13,7 @@ before_fork do ||
   defined?(ActiveRecord::Base) && ActiveRecord::Base.connection.disconnect!
 end
 
-after_fork do ||
+after_fork do |_, _|
   Signal.trap 'TERM' do
     Rails.logger.info 'Unicorn worker intercepting TERM and
       doing nothing. Wait for master to send QUIT'
