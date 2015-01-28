3fec34a7e093b68e715768c925d92c5a08f9e67b
Jakub Novotny <novotny.jakub@gmail.com>
2015-01-28 11:00:28 +0000

    Add configurable e-mail host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,9 @@ Rails.application.configure do
     domain: 'heroku.com',
     enable_starttls_auto: true
   }
+  config.action_mailer.default_url_options = {
+    host: ENV['ACTION_MAILER_HOST']
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
