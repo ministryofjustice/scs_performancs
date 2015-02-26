a8b156811eef5d1f73033ec3b2348498f72a0887
Rob McKinnon <rob.mckinnon@digital.justice.gov.uk>
2015-02-26 13:58:18 +0000

    Change regexp syntax.

--- features/support/ui/pages/edit_profile.rb
+++ features/support/ui/pages/edit_profile.rb
@@ -4,7 +4,7 @@ module UI
   module Pages
     class EditProfile < SitePrism::Page
       set_url '/profile/edit'
-      set_url_matcher(/\/profle\/edit$/)
+      set_url_matcher(%r{\/profle\/edit$})
 
       section :form, UI::Sections::UserForm, 'form'
     end
