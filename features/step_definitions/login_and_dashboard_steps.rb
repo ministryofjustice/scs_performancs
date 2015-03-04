15942e07d9b20a2b80e1b94e3a0bcd64d8cdfa00
Rob McKinnon <rob.mckinnon@digital.justice.gov.uk>
2015-03-04 15:20:50 +0000

    Fix feature expectation.

--- features/step_definitions/login_and_dashboard_steps.rb
+++ features/step_definitions/login_and_dashboard_steps.rb
@@ -22,7 +22,6 @@ Then(/^I should see performance report approval dates$/) do
   expect(page.end_year_approval.first.text).to eql('awaiting approval')
 end
 
-
 And(/^I should see performance reports of my employees$/) do
   page = UI::Pages::Dashboard.new
   page.displayed?
