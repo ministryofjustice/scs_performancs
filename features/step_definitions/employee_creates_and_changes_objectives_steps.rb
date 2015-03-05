0c5915778edf846ca1fd8c14042926330ad8f9d5
Rob McKinnon <rob.mckinnon@digital.justice.gov.uk>
2015-03-05 11:49:36 +0000

    Move steps to new file.

---
+++ features/step_definitions/employee_creates_and_changes_objectives_steps.rb
@@ -0,0 +1,45 @@
+When(/^I create new report with some objectives$/) do
+  @page = UI::Pages::NewReport.new
+  @page.load
+
+  @page.form.smart_objective_what_field_1.set 'Learn english'
+  @page.form.smart_objective_how_field_1.set 'Read children books'
+
+  @page.form.add_objective.click
+  @page.form.add_objective.click
+
+  @page.form.smart_objective_what_field_3.set 'Bake pies'
+
+  @page.form.development_objective_field_1.set 'Objective 1'
+
+  @page.form.add_development.click
+  @page.form.add_development.click
+  @page.form.add_development.click
+  @page.form.add_development.click
+  @page.form.development_objective_field_5.set 'Objective 5'
+
+  @page.form.save_button.click
+end
+
+Then(/^the report is saved$/) do
+  report = Report.first
+
+  expected_development = [
+    'Objective 1', '', '', '', 'Objective 5', '', '', '', '', ''
+  ]
+  expect(report.development).to match_array(expected_development)
+
+  expected_smart = [
+    { 'what' => 'Learn english', 'how' => 'Read children books' },
+    { 'what' => '', 'how' => '' },
+    { 'what' => 'Bake pies', 'how' => '' },
+    { 'what' => '', 'how' => '' },
+    { 'what' => '', 'how' => '' },
+    { 'what' => '', 'how' => '' },
+    { 'what' => '', 'how' => '' },
+    { 'what' => '', 'how' => '' },
+    { 'what' => '', 'how' => '' },
+    { 'what' => '', 'how' => '' }
+  ]
+  expect(report.smart).to eql(expected_smart)
+end
